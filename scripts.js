6e055f15e3836b317b3945f74bfe8303e932ad92
Henrique Hoelscher <H_Hoelscher@hotmail.com>
2021-08-22 22:33:25 -0300

    Create initial styles

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function sum(firstValue, secondValue) {
+    return firstValue + secondValue
+}
+
+function sub(firstValue, secondValue) {
+    return firstValue - secondValue
+}
+
+function mult(firstValue, secondValue) {
+    return firstValue * secondValue
+}
+
+function div(firstValue, secondValue) {
+    return firstValue / secondValue
+}
+
+const buttons  = document.querySelectorAll('.button')
+const viewAction = document.querySelector('.action')
+
